9f53b0c51622536db0219ea9a6bdc0863f9d9f73
SONALI RAWAT <rsona2628@gmail.com>
2025-11-27 11:55:44 +0530

    Print arithmetic progression series
    Removed comment about printing AP series.

---
+++ LOOP PROGRAMS/AP.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+void main()
+{
+int a,d,n,i;
+printf("Enter first term:");
+scanf("%d",&a);
+printf("Enter common difference:");
+scanf("%d",&d);
+printf("Enter no.of terms:");
+scanf("%d",&n);
+printf("AP series=");
+for(i=1;i<=n;i++)
+{ printf("%d, ",a);
+  a=a+d;
+}
+getch();
+}
